2249ef8e9a7acc9dcd79ef7c4638cbbe71d68b2d
a.diesel@gmail.com <a.diesel@gmail.com@7eec36e0-e2f1-11de-8d18-5b59c22968bc>
2010-12-08 13:32:30 +0000

    remove /source

--- branches/my2/plugins/function.new.php
+++ branches/my2/plugins/function.new.php
@@ -13,7 +13,7 @@ function smarty_function_new($params, $template)
     $class = strtolower($params['class']);
     $name = $params['name'];
 
-    $root = realpath(dirname(__FILE__).'/../../');
+    $root = realpath(dirname(__FILE__).'/../');
     require_once($root."/modules/{$class}/{$class}.php");
 
     $obj = new $class();
