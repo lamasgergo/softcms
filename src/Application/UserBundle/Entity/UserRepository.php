d3ca207e016110ad547733b0f0b29b871dbd3a12
a.diesel <a.diesel@gmail.com>
2011-01-28 12:31:58 +0000

    login

--- src/Application/UserBundle/Entity/UserRepository.php
+++ src/Application/UserBundle/Entity/UserRepository.php
@@ -3,9 +3,40 @@
 namespace Application\UserBundle\Entity;
 
 use Symfony\Component\Security\User\UserProviderInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\User\AccountInterface;
 
-use Symfony\Bundle\DoctrineBundle\Security\EntityUserProvider;
+class UserRepository extends EntityRepository implements UserProviderInterface {
 
-class UserRepository extends EntityUserProvider implements UserProviderInterface {
-    
+    /**
+     * Loads the user for the account interface.
+     *
+     * It is up to the implementation if it decides to reload the user data
+     * from the database, or if it simply merges the passed User into the
+     * identity map of an entity manager.
+     *
+     * @throws UnsupportedAccountException if the account is not supported
+     * @param AccountInterface $user
+     *
+     * @return AccountInterface
+     */
+    function loadUserByAccount(AccountInterface $user) {
+        return $this->_em->find($user, $user->getUsername());
+//        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     * @param string $username The username
+     *
+     * @return AccountInterface
+     */
+    function loadUserByUsername($username) {
+        return $this->findOneBy(array('username' => $username));
+    }
 }
